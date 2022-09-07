3965a2291062950bb31cda5c209ec2503fb6a36b
yale1995 <yale850@gmail.com>
2022-09-07 10:20:00 -0300

    adicionando staticPaths

---
+++ pages/produtos/[id].js
@@ -0,0 +1,36 @@
+function Produtos(props) {
+    return <div>Id do produto: {props.id}</div>
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [{
+            params: {
+                id: '1'
+            }
+        },{
+            params: {
+                id: '2'
+            }
+        }
+    ],
+        fallback: 'blocking'
+    }
+}
+
+export async function getStaticProps(context) {
+    await delay(5000)
+    const id = context.params.id
+
+    return {
+        props: {
+            id: id
+        }
+    }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export default Produtos
