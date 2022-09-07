9d83976c0debc1d005988c606ef1548354cd0202
yale1995 <yale850@gmail.com>
2022-09-07 03:14:53 -0300

    adicionando cache de 10s

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,18 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date()
 
+    const endereco = await fetch("https://viacep.com.br/ws/59090900/json/")
+    const enderecoJson = await endereco.json()
+    const {logradouro, bairro} = enderecoJson
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
+
     response.json({
-       date: dynamicDate.toGMTString() 
+       date: dynamicDate.toGMTString() ,
+       endereço: {
+        logradouro,
+        bairro
+       }
     })
 }
 
